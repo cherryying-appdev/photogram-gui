7f38779a09c8a35e28010879f14eb6e4c620bbab
yue-cherry-ying <cherryying@uchicago.edu>
2020-11-08 21:36:41 +0000

    photo create works?

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -6,6 +6,38 @@ class PhotosController < ApplicationController
   end
 
   def show
-    
+    # Parameters: {"path_id"=>"777"}
+    url_id = params.fetch("path_id")
+    matching_photos = Photo.where({ :id => url_id })
+    @the_photo = matching_photos.at(0)
+    render({ :template => "photo_templates/show.html.erb"})
+  end
+
+  def delete
+    # Parameters: {"toast_id"=>"777"}
+    the_id = params.fetch("toast_id")
+    matching_photos = Photo.where({ :id => the_id })
+    the_photo = matching_photos.at(0)
+    the_photo.destroy
+    # render({ :template => "photo_templates/delete.html.erb"})
+    redirect_to("/photos")
+  end
+
+  def create
+    # Parameters: {"query_image"=>"https://www.chicagobooth.edu/why-booth/global-footprint/europe","query_caption"=>"Chicago Booth","query_owner_id"=>"117"}
+    input_image = params.fetch("query_image")
+    input_caption = params.fetch("query_caption")
+    input_owner_id = params.fetch("query_owner_id")
+
+    a_new_photo = Photo.new
+
+    a_new_photo.image = input_image
+    a_new_photo.caption = input_caption
+    a_new_photo.owner_id = input_owner_id
+
+    a_new_photo.save
+
+    # render({ :template => "photo_templates/create.html.erb"})
+    redirect_to("/photos/" + a_new_photo.id.to_s)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   get("/users", { :controller => "users", :action => "index" })
   get("/users/:path_username", { :controller => "users", :action => "show" })
   get("/photos", { :controller => "photos", :action => "index" })
-  get("/photos/:photo_id", { :controller => "photos", :action => "show" })
+  get("/photos/:path_id", { :controller => "photos", :action => "show" })
+  get("/delete_photo/:toast_id", { :controller => "photos", :action => "delete" })
+  get("/insert_photo", { :controller => "photos", :action => "create" })
 end
