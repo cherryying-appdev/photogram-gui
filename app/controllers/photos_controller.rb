207c2a11f2d0254eb6e81e2769f58662c2dd11c5
yue-cherry-ying <cherryying@uchicago.edu>
2020-11-09 05:05:32 +0000

    finished with the assignment

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -59,4 +59,21 @@ class PhotosController < ApplicationController
     next_url = "/photos/" + the_photo.id.to_s
     redirect_to(next_url)
   end
+
+  def insert_comment
+    input_photo_id = params.fetch("input_photo_id")
+    input_author_id = params.fetch("input_author_id")
+    input_comment = params.fetch("input_body")
+
+    a_new_comment = Comment.new
+
+    a_new_comment.photo_id = input_photo_id
+    a_new_comment.author_id = input_author_id
+    a_new_comment.body = input_comment
+
+    a_new_comment.save
+
+    next_url = "/photos/" + a_new_comment.photo_id.to_s
+    redirect_to(next_url)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,13 @@ Rails.application.routes.draw do
   get("/users", { :controller => "users", :action => "index" })
   get("/", { :controller => "users", :action => "index" })
   get("/users/:path_username", { :controller => "users", :action => "show" })
+    get("/insert_user_record", { :controller => "users", :action => "insert" })
+  get("/update_user/:user_id", { :controller => "users", :action => "update" })
+
   get("/photos", { :controller => "photos", :action => "index" })
   get("/photos/:path_id", { :controller => "photos", :action => "show" })
   get("/delete_photo/:toast_id", { :controller => "photos", :action => "delete" })
   get("/insert_photo", { :controller => "photos", :action => "create" })
   get("/update_photo/:modify_id", { :controller => "photos", :action => "update" })
-  get("/insert_user_record", { :controller => "users", :action => "insert" })
-  get("/update_user/:user_id", { :controller => "users", :action => "update" })
+  get("/insert_comment_record", { :controller => "photos", :action => "insert_comment" })
 end
